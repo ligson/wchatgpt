c8baa8911f4f189bc0542f06ef21330d47ff78d8
lijinsheng <lijsd@yonyou.com>
2023-04-24 21:50:38 +0800

    socketChannel

--- build-docker.sh
+++ build-docker.sh
@@ -3,3 +3,4 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-17.jdk/Contents/Home
 mvn clean
 rm -rf target/*
 mvn package docker:build -Dmaven.test.skip=true
+docker push ligson/wchatgpt:springboot-1.0-SNAPSHOT
