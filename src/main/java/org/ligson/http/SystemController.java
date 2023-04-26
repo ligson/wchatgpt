0b103aa07b164935ab9042e61770026793595a90
lijinsheng <lijsd@yonyou.com>
2023-04-26 16:27:12 +0800

    socketChannel

---
+++ src/main/java/org/ligson/http/SystemController.java
@@ -0,0 +1,24 @@
+package org.ligson.http;
+
+import org.ligson.vo.WebResult;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sys")
+public class SystemController {
+    @Value("${app.customer.wx.id}")
+    private String customerId;
+    @Value("${app.customer.wx.qr_code}")
+    private String customerQrCode;
+
+    @GetMapping("/customerInfo")
+    public WebResult customer() {
+        WebResult webResult = WebResult.newSuccessInstance();
+        webResult.putData("wxId", customerId);
+        webResult.putData("wxQrCode", customerQrCode);
+        return webResult;
+    }
+}
