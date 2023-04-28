471cc029e1bb35b45f673dde0f8c8526d9b34474
jinmancang1 <jinmancang1@jd.com>
2023-04-28 11:19:31 +0800

    fix: 快速增加用户

---
+++ batch-add-user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#domain=https://ichat.x-assn.xyz
+
+while read line
+do
+  curl --location --request POST $domain'/api/auth/register' \
+--header 'Content-Type: application/json' \
+--data-raw '{ "username": "'$line'", "password": "'$line'","register_code":"upgrop@1949" }'
+
+curl --location --request POST $domain'/api/user/upgrade' \
+--header 'Content-Type: application/json' \
+--data-raw '{ "username": "'$line'","register_code":"upgrop@1949" }'
+done < add_users.txt
+
