4768c4cbae7d385242c208c958335b1128c8e6ce
jinmancang1 <jinmancang1@jd.com>
2023-05-15 22:22:09 +0800

    fix: 优化代码

---
+++ src/main/java/org/ligson/ichat/service/impl/CacheServiceImpl.java
@@ -0,0 +1,53 @@
+package org.ligson.ichat.service.impl;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.ligson.ichat.domain.User;
+import org.ligson.ichat.serializer.CruxSerializer;
+import org.ligson.ichat.service.CacheService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class CacheServiceImpl implements CacheService {
+
+    private final static String USER_SESSION_CONTEXT_PREFIX = "xchat:session-context:";
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private CruxSerializer cruxSerializer;
+
+    @Override
+    public User getLoginUserByToken(String token) {
+        if (StringUtils.isBlank(token)) {
+            return null;
+        }
+        String userJson = stringRedisTemplate.boundValueOps(USER_SESSION_CONTEXT_PREFIX + ":token-user:" + token).get();
+        if (StringUtils.isNotBlank(userJson)) {
+            return cruxSerializer.deserialize(userJson, User.class);
+        }
+        return null;
+    }
+
+    @Override
+    public int getLoginUserTerminalCount(String userId) {
+        Map<Object, Object> userTokensMap = stringRedisTemplate.boundHashOps(USER_SESSION_CONTEXT_PREFIX + ":user-tokens:" + userId).entries();
+        return userTokensMap == null ? 0 : userTokensMap.values().size();
+    }
+
+    @Override
+    public void setUserAndToken(String token, User user) {
+        stringRedisTemplate.boundHashOps(USER_SESSION_CONTEXT_PREFIX + ":user-tokens:" + user.getId()).put(token, DateFormatUtils.format(new Date(), "yyyyMMddHHmmss"));
+        stringRedisTemplate.boundValueOps(USER_SESSION_CONTEXT_PREFIX + ":token-user:" + token).set(cruxSerializer.serialize(user));
+    }
+
+    @Override
+    public void relieveUserAndToken(String token, User user) {
+        stringRedisTemplate.boundHashOps(USER_SESSION_CONTEXT_PREFIX + ":user-tokens:" + user.getId()).delete(token);
+        stringRedisTemplate.delete(USER_SESSION_CONTEXT_PREFIX + ":token-user:" + token);
+    }
+}
--- src/main/java/org/ligson/ichat/service/impl/UserServiceImpl.java
+++ src/main/java/org/ligson/ichat/service/impl/UserServiceImpl.java
@@ -4,22 +4,19 @@ package org.ligson.ichat.service.impl;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.apache.commons.lang3.time.DateFormatUtils;
 import org.ligson.ichat.dao.UserDao;
 import org.ligson.ichat.domain.User;
 import org.ligson.ichat.enums.UserLevel;
-import org.ligson.ichat.serializer.CruxSerializer;
+import org.ligson.ichat.service.CacheService;
 import org.ligson.ichat.service.UserService;
 import org.ligson.ichat.vo.RegisterDTO;
 import org.ligson.ichat.vo.WebResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.Map;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -38,40 +35,28 @@ public class UserServiceImpl implements UserService {
     @Value("${app.server.register-code}")
     private String registerCode;
 
-    private final static String USER_SESSION_CONTEXT_PREFIX = "xchat:session-context:";
-    //已经登录用户
-
     @Autowired
-    private StringRedisTemplate stringRedisTemplate;
-    @Autowired
-    private CruxSerializer cruxSerializer;
+    private CacheService cacheService;
 
     public User getLoginUserByToken(String token) {
-        if (StringUtils.isBlank(token)) {
-            return null;
-        }
-        String userJson = stringRedisTemplate.boundValueOps(USER_SESSION_CONTEXT_PREFIX + ":token-user:" + token).get();
-        if (StringUtils.isNotBlank(userJson)) {
-            return cruxSerializer.deserialize(userJson, User.class);
-        }
-        return null;
+        return cacheService.getLoginUserByToken(token);
     }
 
     public WebResult login(String username, String password) {
         WebResult webResult = new WebResult();
-        User vo = userDao.findByName(username);
-        if (vo != null) {
-            Map<Object, Object> userTokensMap = stringRedisTemplate.boundHashOps(USER_SESSION_CONTEXT_PREFIX + ":user-tokens:" + vo.getId()).entries();
-            int count = userTokensMap == null ? 0 : userTokensMap.values().size();
+        User user = userDao.findByName(username);
+        if (user != null) {
+            int count = cacheService.getLoginUserTerminalCount(user.getId());
+            ;
             if (count > 30) {
                 webResult.setSuccess(false);
                 webResult.setErrorMsg("最多可以使用两个设备登录，可以退出之前登录后再试");
                 return webResult;
             }
 
-            if (vo.getPassword().equals(password)) {
-                if (vo.getTimes() <= 0) {
-                    if (vo.getLevel() == UserLevel.FREE) {
+            if (user.getPassword().equals(password)) {
+                if (user.getTimes() <= 0) {
+                    if (user.getLevel() == UserLevel.FREE) {
                         webResult.setSuccess(false);
                         webResult.setErrorMsg("成本有限,免费用户只能体验20次，请联系管理员付费,请谅解");
                     } else {
@@ -80,14 +65,13 @@ public class UserServiceImpl implements UserService {
                     }
                     return webResult;
                 }
-                vo.setLastedLoginTime(new Date());
-                userDao.update(vo);
+                user.setLastedLoginTime(new Date());
+                userDao.update(user);
                 String token = UUID.randomUUID().toString();
                 webResult.setSuccess(true);
                 webResult.putData("username", username);
                 webResult.putData("token", token);
-                stringRedisTemplate.boundHashOps(USER_SESSION_CONTEXT_PREFIX + ":user-tokens:" + vo.getId()).put(token, DateFormatUtils.format(new Date(), "yyyyMMddHHmmss"));
-                stringRedisTemplate.boundValueOps(USER_SESSION_CONTEXT_PREFIX + ":token-user:" + token).set(cruxSerializer.serialize(vo));
+                cacheService.setUserAndToken(token, user);
             } else {
                 webResult.setSuccess(false);
                 webResult.setErrorMsg("密码错误");
@@ -216,8 +200,7 @@ public class UserServiceImpl implements UserService {
         if (StringUtils.isNotBlank(token)) {
             User user = getLoginUserByToken(token);
             if (user != null) {
-                stringRedisTemplate.boundHashOps(USER_SESSION_CONTEXT_PREFIX + ":user-tokens:" + user.getId()).delete(token);
-                stringRedisTemplate.delete(USER_SESSION_CONTEXT_PREFIX + ":token-user:" + token);
+                cacheService.relieveUserAndToken(token, user);
             }
         }
         return WebResult.newSuccessInstance();
